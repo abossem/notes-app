f17f6257a911353a9a2b19064f862e09d4b834b3
abdelrahman assem <abdelrahmanassem222@gmail.com>
2025-04-12 15:14:49 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import LoginPage from "./LoginPage";
-import Signup from "./SignupPage";
-
 function App() {
   return (
-    // <div>
-    //   <h1>Notes</h1>
-    // </div>
-
-    // <LoginPage />
-
-    <Signup />
+    <div>
+      <h1>Notes</h1>
+    </div>
   );
 }
 
